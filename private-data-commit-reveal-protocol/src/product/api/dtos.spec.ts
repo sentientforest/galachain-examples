6a7ac2c3cd9e4d8d664340dd27cd6ccebd56197e
Mike Graham <michael@forested.earth>
2025-06-04 18:17:35 -0700

    test: commit-reveal dto tests wip

--- private-data-commit-reveal-protocol/src/product/api/dtos.spec.ts
+++ private-data-commit-reveal-protocol/src/product/api/dtos.spec.ts
@@ -23,12 +23,30 @@ import {
 } from "./dtos";
 
 describe("dtos", () => {
+  const testHash = bytesToHex(sha256(utf8ToBytes("test")));
+
   it("should construct CommitSubmissionDto with constructor", async () => {
     // Given
     const dto = new CommitSubmissionDto({
       collection: "test collection",
-      hash: bytesToHex(sha256(utf8ToBytes("test"))),
+      hash: testHash,
       uniqueKey: "test nonce"
+    });
+
+    // When
+    const validation = await dto.validate();
+
+    // Then
+    expect(validation).toEqual([]);
+  });
+
+  it("should construct CommitSubmissionResDto with plainToInstance", async () => {
+    // Given
+    const dto = plainToInstance(CommitSubmissionResDto, {
+      collection: "test collection",
+      owner: asValidUserRef("client|abc"),
+      hash: testHash,
+      nonce: "0"
     });
 
     // When
